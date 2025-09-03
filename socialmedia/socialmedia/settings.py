f2565bf23ad18b4982dc9a0ef56fa35cd177bd03
abhiram3107 <108091693+abhiram3107@users.noreply.github.com>
2025-09-03 15:41:52 +0530

    added_git_ignore

--- socialmedia/socialmedia/settings.py
+++ socialmedia/socialmedia/settings.py
@@ -92,12 +92,8 @@ WSGI_APPLICATION = 'socialmedia.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'devconnect_db', 
-        'USER': 'root',  
-        'PASSWORD': 'root', 
-        'HOST': 'localhost',
-        'PORT': '3306', 
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
